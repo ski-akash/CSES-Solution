1d64de69907606cd9053df8ab523961c306a9c29
sky <ski.akashh@gmail.com>
2026-05-26 01:25:54 +0530

    Undefined Behaviour Fixed

--- Counting_Numbers.cpp
+++ Counting_Numbers.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int64_t dp[20][2][10][2];
+int64_t dp[20][2][11][2];
 
 int64_t dfs(int pos, int tight, int prev, int lz, string digit){
     // base case ::
@@ -30,7 +30,7 @@ int main(){
     int64_t a,b; cin >> a >> b;
 
     memset(dp,-1,sizeof(dp));
-    int64_t B = dfs(0,1,-1,1,to_string(b));
+    int64_t B = dfs(0,1,10,1,to_string(b));
 
     if(a == 0){
         cout << B << endl;
@@ -38,7 +38,7 @@ int main(){
     }
 
     memset(dp,-1,sizeof(dp));
-    int64_t A = dfs(0,1,-1,1,to_string(a-1));
+    int64_t A = dfs(0,1,10,1,to_string(a-1));
 
     cout << B - A << endl;
     return 0;
